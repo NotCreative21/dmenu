3cac148b34d4dc924b28327ec69e48e17eb3b621
NotCreative21 <exoplasm1@gmail.com>
2021-11-30 16:59:27 -0500

    updated colors

--- config.h
+++ config.h
@@ -4,14 +4,14 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"terminus:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#9abada", "#222832" },
+	[SchemeSel] = { "#9abada", "#363E51" },
+	[SchemeOut] = { "#9abada", "#222832" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 10;

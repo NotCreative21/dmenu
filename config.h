1bc1945dc51a55b1a875a67c41b71f361d5b7d81
NotCreative21 <seanray410@gmail.com>
2021-12-11 02:22:23 -0500

    color change

--- config.h
+++ config.h
@@ -4,14 +4,19 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"terminus:size=10"
+	"monospace:size=8"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+
+static const char col_gray1[]       = "#282828";
+static const char col_gray3[]       = "#ebdbb2";
+static const char col_cyan[]        = "#928374";
+
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#9abada", "#222832" },
-	[SchemeSel] = { "#9abada", "#363E51" },
-	[SchemeOut] = { "#9abada", "#222832" },
+	[SchemeNorm] = { col_gray3, col_gray1 },
+	[SchemeSel] = { col_gray3, col_cyan },
+	[SchemeOut] = { col_gray3, col_gray1 },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 10;

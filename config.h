76ed5956b884ea801b168b9ba5c1228de1c7bfc5
NotCreative21 <seanray410@gmail.com>
2022-01-27 20:56:23 -0500

    adjust sizing

--- config.h
+++ config.h
@@ -4,13 +4,14 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=8"
+	"JetBrainsMonoMedium Nerd Font:size=9",
+	"FiraCode-Regular:pixelsize=9:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 
-static const char col_gray1[]       = "#282828";
-static const char col_gray3[]       = "#ebdbb2";
-static const char col_cyan[]        = "#928374";
+static const char col_gray1[]       = "#1d1f21";
+static const char col_gray3[]       = "#707880";
+static const char col_cyan[]        = "#1d1f21";
 
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
@@ -19,8 +20,8 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeOut] = { col_gray3, col_gray1 },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 10;
-static unsigned int columns    = 4;
+static unsigned int lines      = 12;
+static unsigned int columns    = 5;
 
 /*
  * Characters not considered part of a word while deleting words

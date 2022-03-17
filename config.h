6001f90814e7e3f71812d1918c4d9d87f78a96e1
NotCreative21 <seanray410@gmail.com>
2022-03-16 19:38:45 -0500

    config update

--- config.h
+++ config.h
@@ -4,24 +4,25 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"JetBrainsMonoMedium Nerd Font:size=9",
-	"FiraCode-Regular:pixelsize=9:antialias=true:autohint=true"
+	"TerminessTTF Nerd Font:pixelsize=12",
+	"FiraCode-Regular:pixelsize=9:antialias=true:autohint=true",
+	"JoyPixels:size=9"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 
-static const char col_gray1[]       = "#1d1f21";
-static const char col_gray3[]       = "#707880";
-static const char col_cyan[]        = "#1d1f21";
+static const char col_gray1[]       = "#282828";
+static const char col_gray3[]       = "#a89984";
+static const char col_cyan[]        = "#83a598";
 
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { col_gray3, col_gray1 },
-	[SchemeSel] = { col_gray3, col_cyan },
+	[SchemeSel] = { col_cyan, col_gray1 },
 	[SchemeOut] = { col_gray3, col_gray1 },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 12;
-static unsigned int columns    = 5;
+static unsigned int lines      = 10;
+static unsigned int columns    = 2;
 
 /*
  * Characters not considered part of a word while deleting words

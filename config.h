091f3be53514c24550a1b895b5e2c2dee718a401
NotCreative21 <seanray410@gmail.com>
2022-07-16 16:52:35 -0500

    add border

--- config.h
+++ config.h
@@ -4,20 +4,21 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"TerminessTTF Nerd Font:pixelsize=12",
+	"JetBrains Mono:pixelsize=12:antialiasing=true",
 	"FiraCode-Regular:pixelsize=9:antialias=true:autohint=true",
 	"JoyPixels:size=9"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 
-static const char col_gray1[]       = "#282828";
-static const char col_gray3[]       = "#a89984";
-static const char col_cyan[]        = "#83a598";
+static const char col_gray1[]       = "#27282d";
+static const char col_gray2[]       = "#6b7b88";
+static const char col_gray3[]       = "#c9d2e4";
+static const char col_cyan[]        = "#9abada";
 
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { col_gray3, col_gray1 },
-	[SchemeSel] = { col_cyan, col_gray1 },
+	[SchemeSel] = { col_cyan, col_gray2 },
 	[SchemeOut] = { col_gray3, col_gray1 },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
@@ -31,4 +32,4 @@ static unsigned int columns    = 2;
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static const unsigned int border_width = 2;
+static const unsigned int border_width = 1;
